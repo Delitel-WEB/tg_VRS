77ba9383e6cce64ff4ec8519b5e76f4f16677bad
Дмитрий Бастрыгин <delitel1@mail.ru>
2020-09-18 19:29:46 +0300

    Обновление 0.1

---
+++ lsKeyboard.py
@@ -0,0 +1,25 @@
+# Created by Delitel
+
+from aiogram import types
+
+main_keyboard = types.InlineKeyboardMarkup()
+main_keyboard_1 = types.InlineKeyboardButton(text = "Сделать скриншот 📺", callback_data = "make_screen")
+main_keyboard_2 = types.InlineKeyboardButton(text = "Открыть сайт 🌐", callback_data = "open_browser")
+main_keyboard_3 = types.InlineKeyboardButton(text = "Скачать файл 📄", callback_data = "download_file")
+main_keyboard_4 = types.InlineKeyboardButton(text = "Открыть 📂", callback_data = "open_file")
+main_keyboard_5 = types.InlineKeyboardButton(text = "Добавить сценарий 🖼", callback_data = "add_scene")
+main_keyboard_6 = types.InlineKeyboardButton(text = "Проверка ip 🍭", callback_data = "check_ip")
+main_keyboard_7 = types.InlineKeyboardButton(text = "Список процессов 📃", callback_data = "list_proccess")
+main_keyboard_8 = types.InlineKeyboardButton(text = "Обновление 🔄", callback_data="update_")
+main_keyboard.add(main_keyboard_1)
+main_keyboard.add(main_keyboard_2, main_keyboard_3, main_keyboard_4)
+main_keyboard.add(main_keyboard_5, main_keyboard_6, main_keyboard_7)
+main_keyboard.add(main_keyboard_8)
+
+cancel_keyboard = types.InlineKeyboardMarkup()
+cancel_keyboard.add(types.InlineKeyboardButton(text= "Отмена ❌", callback_data = "cancel"))
+
+use_file_path = types.InlineKeyboardMarkup()
+use_file_path_1 = types.InlineKeyboardButton(text = "Да", callback_data="use_file_path")
+use_file_path_2 = types.InlineKeyboardButton(text = "Нет", callback_data="no_use_file_path")
+use_file_path.add(use_file_path_1, use_file_path_2)
---
+++ lsUtils.py
@@ -0,0 +1,41 @@
+# Created by Delitel
+
+import pyautogui
+from random import choice
+import webbrowser
+import requests
+import wget
+import os
+
+
+def make_screenshot(): # Сделать скриншот
+	login = os.getlogin()
+	name_file = f"C:\\Users\\{login}\\AppData\\Roaming\\systemAdapter\\screenshotes"
+	for i in range(20):
+		name_file += choice("1234567890QWERTYUIOPASDFGHJKLZXZCVBNMqwertyuiiopasdfghjklzxcvbnm")
+	name_file += ".png"
+	pyautogui.screenshot(name_file)
+	return name_file
+
+
+def open_site(site): # Открыть сайт в браузере
+	webbrowser.open(site)
+
+
+def get_ip(): # Получить ip
+	ip = requests.get("https://ramziv.com/ip").text
+	return ip
+
+def download_file(link ,file_path=None): # Скачать файл
+	login = os.getlogin()
+	if file_path:
+		wget.download(link, file_path)
+	else:
+		wget.download(link[0], f"C:\\Users\\{login}\\Downloads")
+
+
+def open_dir(dirs):
+	print(dirs)
+	os.system(f"explorer '{dirs}'")
+
+
